3457e3461c4f138d6687353776bf769ddcd0f5de
Bergur Snorrason <bsnorrason@gmail.com>
2023-03-02 21:47:38 +0000

    -

--- random-heap.c
+++ random-heap.c
@@ -5,17 +5,17 @@ typedef struct { int v, l, r; } rheap; rheap rheaps[MAXN];
 
 int rheap_init(int v)
 {
-	rheaps[rheap_c].l = rheaps[rheap_c].r = -1, rheaps[rheap_c].v = v;
-	return rheap_c++;
+    rheaps[rheap_c].l = rheaps[rheap_c].r = -1, rheaps[rheap_c].v = v;
+    return rheap_c++;
 }
 
 int rheap_merge(int x, int y)
 {
-	if (x == -1 || y == -1) return x != -1 ? x : y;
-	if (b[rheaps[y].v] < b[rheaps[x].v]) { int s = x; x = y; y = s; }
-	if (rand()&1) { int s = rheaps[x].l; rheaps[x].l = rheaps[x].r; rheaps[x].r = s; }
-	rheaps[x].l = rheap_merge(rheaps[x].l, y);
-	return x;
+    if (x == -1 || y == -1) return x != -1 ? x : y;
+    if (b[rheaps[y].v] < b[rheaps[x].v]) { int s = x; x = y; y = s; }
+    if (rand()&1) { int s = rheaps[x].l; rheaps[x].l = rheaps[x].r; rheaps[x].r = s; }
+    rheaps[x].l = rheap_merge(rheaps[x].l, y);
+    return x;
 }
 int rheap_push(int x, int v) { return rheap_merge(rheap_init(v), x); }
 int rheap_peek(int x) { return rheaps[x].v; }
