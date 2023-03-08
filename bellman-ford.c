5730d0984384594113c8f49fcc60707eac464109
Bergur Snorrason <bsnorrason@gmail.com>
2023-03-08 12:52:44 +0000

    -

--- bellman-ford.c
+++ bellman-ford.c
@@ -1,15 +1,10 @@
-void bellman_ford(ll **g, ll n, ll m, ll s, ll *d)
+void bellman_ford(ll **g, int n, int m, int s, int *d)
 {
-    ll i, j, k, x, w, q[n];
+    int i, j, k, x, w, q[n], f = 1;
     rep(i, n) d[i] = i == s ? 0 : INF, q[i] = i == s ? 0 : -1;
-    rep(i, n - 1) rep(j, n) if (q[j] == i) rep(k, g[j][-1])
+    for (i = 0; f; i++) for (j = f = 0; j < n; j++) if (q[j] == i) rep(k, g[j][-1])
     {
         x = next(g[j][k]), w = weight(g[j][k]);
-        if (d[j] + w < d[x]) d[x] = d[j] + w, q[x] = i + 1;
-    }
-    rep(i, n - 1) rep(j, n) if (q[j] == n + i - 1) rep(k, g[j][-1])
-    {
-        x = next(g[j][k]), w = weight(g[j][k]);
-        if (d[x] != -INF && d[j] + w < d[x]) d[x] = -INF, q[x] = n + i;
+        if (d[x] != -INF && d[j] + w < d[x]) d[x] = i < n ? d[j] + w : -INF, q[x] = i + 1, f = 1;
     }
 }
