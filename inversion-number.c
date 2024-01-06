715bda359032a61c8971d7d019d0c0f89f3ec037
Bergur Snorrason <bsnorrason@gmail.com>
2024-01-06 17:48:08 +0000

    .

--- inversion-number.c
+++ inversion-number.c
@@ -1,8 +1,19 @@
 // field testing: froshweek, ultraquicksort
-ll invnum(ll* a, ll n)
+
+// Usage:  x = invnum(a, n);
+// Before: a in an array of length n.
+// Usage:  x is the inversion number of a.
+// Note:   The "inversion number" of a is the number
+//           of adjacent swaps needed to sort a.
+//         It runs in O(n log n).
+//         It is fast for large n (less than 1s. for n = 10^6),
+//           but the answer grows like n^2, so it will overflow
+//           32 bits.
+
+int invnum(int* a, int n)
 {
-    ll t = 0, z = 0, i, j, c, e, l, m, r, b[2][n];
-    for (i = 0; i < n; i++) b[t][i] = a[i];
+    int t = 0, z = 0, i, j, c, e, l, m, r, b[2][n];
+    rep(i, n) b[t][i] = a[i];
     for (e = 1; e < n; e *= 2, t = 1 - t) for (l = 0; l < n; l += 2*e)
     {
         i = c = l, j = m = min(l + e, n), r = min(l + 2*e, n);
