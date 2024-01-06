6950b2149645aaa0b461ed872f61849a0e3896ff
Bergur Snorrason <bsnorrason@gmail.com>
2024-01-06 17:40:48 +0000

    .

--- lis.c
+++ lis.c
@@ -1,14 +1,21 @@
 // retesting NEEDED
 // field testing trainsorting, manhattanmornings, signals
+
+// Usage:  x = lis(a, b, n);
+// Before: a is an array of length n.
+// After:  x is the length of the longest incr. suseqs.
+//         b is an array that contains an incr. subsec. of a of length x.
+// Note:   It runs in O(n log n).
+
 int lis(int *a, int *b, int n)
 {
-    int i, j, x, y, d[n + 1], e[n]
-        for (i = 0; i < n + 1; i++) d[i] = i == 0 ? -INF : INF;
+    int i, j, x, y, d[n + 1], e[n];
+    for (i = 0; i < n + 1; i++) d[i] = i == 0 ? -INF : INF;
     for (i = 0; i < n; i++)
     {
-        int r = -1, s
-            for (s = n + 1; s >= 1; s /= 2)
-                while (r + s < n + 1 && d[r + s] < a[i]) r += s;
+        int r = -1, s;
+        for (s = n + 1; s >= 1; s /= 2)
+            while (r + s < n + 1 && d[r + s] < a[i]) r += s;
         d[r + 1] = a[i], e[i] = d[r];
     }
     for (x = n; d[x] == INF; x--);
