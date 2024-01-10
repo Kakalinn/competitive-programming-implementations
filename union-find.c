4b188ae05d01e5fabcb6c01fc740daf3af30eef2
Bergur Snorrason <bsnorrason@gmail.com>
2024-01-10 14:41:14 +0000

    .

--- union-find.c
+++ union-find.c
@@ -1,9 +1,10 @@
-int uf_find(int *p, int x) { return p[x] < 0 ? x : (p[x] = uf_find(p, p[x])); }
-void uf_init(int n) { int i; rep(i, n) p[i] = -1; }
-void uf_size(int x) { return -p[uf_find(x)]; }
-void uf_join(int *p, int x, int y)
+int find(int *p, int x) { return p[x] < 0 ? x : (p[x] = find(p, p[x])); }
+void init(int *p, int n) { int i; rep(i, n) p[i] = -1; }
+int size(int *p, int x) { return -p[find(p, x)]; }
+int check(int *p, int x, int y) { return find(p, x) == find(p, y); }
+void join(int *p, int x, int y)
 {
-    int rx = uf_find(p, x), ry = uf_find(p, y);
+    int rx = find(p, x), ry = find(p, y);
     if (rx == ry) return;
     if (p[rx] > p[ry]) p[ry] += p[rx], p[rx] = ry;
     else p[rx] += p[ry], p[ry] = rx;
