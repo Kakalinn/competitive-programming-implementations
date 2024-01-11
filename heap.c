fa7bb2613429cfaa72b2e6dcbd995d1143e41658
Bergur Snorrason <bsnorrason@gmail.com>
2024-01-11 09:41:33 +0000

    .

--- heap.c
+++ heap.c
@@ -1,3 +1,27 @@
+
+// Usage:  init(h);
+// Before: h is an array of size n + 1.
+// After:  h is an empty heap with capacity n.
+//
+// Usage:  pop(h);
+// Before: h is a non-empty heap.
+// After:  The largest element in h has been removed.
+//
+// Usage:  push(h, x);
+// Before: h is a non-full heap.
+// After:  x has been added to h.
+//
+// Usage:  x = peek(h);
+// Before: h is a non-empty heap.
+// After:  x is the largest element in h.
+//
+// Usage:  x = size(h);
+// Before: h is a heap.
+// After:  x is the number of elements in h.
+//
+// Note:   init(...), peek(...), and size(...) run in O(1).
+//         pop(...) and push(...) run in O(log n).
+
 void swap(int* x, int* y) { int t = *x; *x = *y; *y = t; }
 void pop(int *h)
 {
