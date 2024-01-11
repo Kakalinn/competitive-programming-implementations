bb81d89c99d2c2b9eb6d6710d17b263dbe7ab0e1
Bergur Snorrason <bsnorrason@gmail.com>
2024-01-11 09:34:54 +0000

    .

--- mex.c
+++ mex.c
@@ -1,3 +1,13 @@
+
+// Usage:  x = mex(a, n);
+// Before: n >= 0 is an integer and a is an array of size n.
+//         No entry in a is smaller than 0.
+// After:  x >= 0 is the smallest excluded number of a.
+//         That is, it is the smallest natural number that
+//         is not in a.
+//
+// Note:   It runs in O(n).
+
 int mex(int* a, int n)
 {
     int b[n], i;
